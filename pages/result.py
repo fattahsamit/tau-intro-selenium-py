107e1c7561931a69beed77a15c124a2d6a51e9ad
fattahsamit <fattahsamit@gmail.com>
2023-07-17 19:10:33 +0600

    finding locators for elements

--- pages/result.py
+++ pages/result.py
@@ -3,8 +3,12 @@ This module contains DuckDuckGoResultPage,
 the page object for the DuckDuckGo search result page.
 """
 
+from selenium.webdriver.common.by import By
+
 
 class DuckDuckGoResultPage:
+    RESULT_LINKS = (By.CSS_SELECTOR, "a.result__a")
+    SEARCH_INPUT = (By.ID, "searchbox_input")
 
     def __init__(self, browser):
         self.browser = browser
