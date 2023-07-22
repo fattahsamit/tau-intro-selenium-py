56deef6a184a8132eee1babcb39b88c181dade18
fattahsamit <fattahsamit@gmail.com>
2023-07-22 21:57:56 +0600

    pytest-xdist to run parallel tests

--- tests/test_search.py
+++ tests/test_search.py
@@ -3,18 +3,19 @@ These tests cover DuckDuckGo searches.
 """
 from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
+import pytest
 
 
-def test_basic_duckduckgo_search(browser):
+@pytest.mark.parametrize('phrase', ['panda', 'python', 'polar bear'])
+def test_basic_duckduckgo_search(browser, phrase):
     search_page = DuckDuckGoSearchPage(browser)
     result_page = DuckDuckGoResultPage(browser)
-    PHRASE = "panda"
 
     # Given the DuckDuckGo home page is displayed
     search_page.load()
 
     # When the user searches for "panda"
-    search_page.search(PHRASE)
+    search_page.search(phrase)
 
     '''
     # Explicit Waits
@@ -26,19 +27,19 @@ def test_basic_duckduckgo_search(browser):
     from selenium.webdriver.support.wait import WebDriverWait
     from selenium.webdriver.support import expected_conditions as EC
     WebDriverWait(browser, 10).until(
-        EC.title_contains(PHRASE+' at DuckDuckGo'))
+        EC.title_contains(phrase+' at DuckDuckGo'))
     '''
 
     # And the search result query is "panda"
-    assert PHRASE == result_page.search_input_value()
+    assert phrase == result_page.search_input_value()
 
     # And the search result links pertain to "panda"
     # titles = result_page.result_link_titles()
-    # matches = [t for t in titles if PHRASE.lower() in t.lower()]
+    # matches = [t for t in titles if phrase.lower() in t.lower()]
     # assert len(matches) > 0
 
     for title in result_page.result_link_titles():
-        assert PHRASE.lower() in title.lower()
+        assert phrase.lower() in title.lower()
 
     # Then the search result title contains "panda"
-    assert PHRASE+' at DuckDuckGo' in result_page.title()
+    assert phrase+' at DuckDuckGo' in result_page.title()
